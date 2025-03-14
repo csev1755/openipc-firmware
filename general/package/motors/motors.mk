1afeb558b76f5992cba61777ff292fcededcae7d
csev1755 <49533775+csev1755@users.noreply.github.com>
2025-03-14 09:04:29 -0500

    make motor options

--- general/package/motors/motors.mk
+++ general/package/motors/motors.mk
@@ -11,19 +11,19 @@ MOTORS_LICENSE = MIT
 MOTORS_LICENSE_FILES = LICENSE
 
 define MOTORS_BUILD_CMDS
-	(cd $(@D)/camhi-motor; $(TARGET_CC) -Os -s main.c -o camhi-motor)
-	(cd $(@D)/i2c-motor; $(TARGET_CC) -Os -s main.c -o i2c-motor)
-	(cd $(@D)/xm-kmotor; $(TARGET_CC) -Os -s main.c -o xm-kmotor)
-	(cd $(@D)/xm-uart; $(TARGET_CC) -Os -s main.c -o xm-uart)
-	(cd $(@D)/ingenic-motor; $(TARGET_CC) -Os -s main.c -o ingenic-motor)
+	$(if $(BR2_PACKAGE_CAMHI_MOTOR), (cd $(@D)/camhi-motor; $(TARGET_CC) -Os -s main.c -o camhi-motor))
+	$(if $(BR2_PACKAGE_I2C_MOTOR), (cd $(@D)/i2c-motor; $(TARGET_CC) -Os -s main.c -o i2c-motor))
+	$(if $(BR2_PACKAGE_XM_KMOTOR), (cd $(@D)/xm-kmotor; $(TARGET_CC) -Os -s main.c -o xm-kmotor))
+	$(if $(BR2_PACKAGE_XM_UART_MOTOR), (cd $(@D)/xm-uart; $(TARGET_CC) -Os -s main.c -o xm-uart))
+	$(if $(BR2_PACKAGE_INGENIC_MOTOR), (cd $(@D)/ingenic-motor; $(TARGET_CC) -Os -s main.c -o ingenic-motor))
 endef
 
 define MOTORS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor
-	$(INSTALL) -m 0755 -D $(@D)/i2c-motor/i2c-motor $(TARGET_DIR)/usr/bin/i2c-motor
-	$(INSTALL) -m 0755 -D $(@D)/xm-kmotor/xm-kmotor $(TARGET_DIR)/usr/bin/xm-kmotor
-	$(INSTALL) -m 0755 -D $(@D)/xm-uart/xm-uart $(TARGET_DIR)/usr/bin/xm-uart
-	$(INSTALL) -m 0755 -D $(@D)/ingenic-motor/ingenic-motor $(TARGET_DIR)/usr/bin/ingenic-motor
+	$(if $(BR2_PACKAGE_CAMHI_MOTOR), $(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor)
+	$(if $(BR2_PACKAGE_I2C_MOTOR), $(INSTALL) -m 0755 -D $(@D)/i2c-motor/i2c-motor $(TARGET_DIR)/usr/bin/i2c-motor)
+	$(if $(BR2_PACKAGE_XM_KMOTOR), $(INSTALL) -m 0755 -D $(@D)/xm-kmotor/xm-kmotor $(TARGET_DIR)/usr/bin/xm-kmotor)
+	$(if $(BR2_PACKAGE_XM_UART_MOTOR), $(INSTALL) -m 0755 -D $(@D)/xm-uart/xm-uart $(TARGET_DIR)/usr/bin/xm-uart)
+	$(if $(BR2_PACKAGE_INGENIC_MOTOR), $(INSTALL) -m 0755 -D $(@D)/ingenic-motor/ingenic-motor $(TARGET_DIR)/usr/bin/ingenic-motor)
 endef
 
 $(eval $(generic-package))
